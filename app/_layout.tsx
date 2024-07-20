ca43700e69762d88e02fc5f756e311ffe99eab71
Henry Fraser <henrymfraser@gmail.com>
2024-07-20 15:04:36 +1000

    Run eslint and prettier

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import '@tamagui/core/reset.css'
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "@tamagui/core/reset.css";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
 import tamaguiConfig from "@/tamagui.config";
-import {TamaguiProvider} from "tamagui";
+import { TamaguiProvider } from "tamagui";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -30,7 +27,7 @@ export default function RootLayout() {
   }
 
   return (
-      <TamaguiProvider config={tamaguiConfig}>
+    <TamaguiProvider config={tamaguiConfig}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
