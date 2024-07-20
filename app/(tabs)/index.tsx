4bfb5d2b777b2ed700a2319203d4aa4b0ad3ef2f
Henry Fraser <henrymfraser@gmail.com>
2024-07-20 17:43:59 +1000

    Add working bedrock calls

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,12 +11,72 @@ import {
 } from "tamagui";
 
 import React, { useEffect, useState } from "react";
+import {
+  BedrockRuntimeClient,
+  InvokeModelCommand,
+  InvokeModelCommandOutput,
+} from "@aws-sdk/client-bedrock-runtime";
+
+export const CLAUDE_MAX_TOKENS = 4096;
+export const APPLICATION_JSON = "application/json";
+export const BEDROCK_VERSION = "bedrock-2023-05-31";
 
 export default function HomeScreen() {
   const [loading, setLoading] = useState<boolean>(false);
   const [question, setQuestion] = useState<string>("");
   const [summary, setSummary] = useState<string>("");
   const [pages, setPages] = useState<number[]>([]);
+
+  const client = new BedrockRuntimeClient({
+    region: process.env.AWS_REGION_NAME || "",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+    },
+  });
+
+  // TODO: consider switching to claude instant, or something cheap
+  const HAIKU_MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0";
+
+  // TODO: optimise claude's message so that it's given a template and provides only the filled responses.
+  const body = JSON.stringify({
+    messages: [
+      {
+        role: "user",
+        content: question,
+      },
+    ],
+    anthropic_version: BEDROCK_VERSION,
+    max_tokens: CLAUDE_MAX_TOKENS,
+    system: "", // TODO:
+  });
+
+  const params = {
+    modelId: HAIKU_MODEL_ID,
+    contentType: APPLICATION_JSON,
+    accept: APPLICATION_JSON,
+    body,
+  };
+
+  const request = new InvokeModelCommand(params);
+
+  const sendToBedrock = async () => {
+    console.log("in sending...");
+    try {
+      console.log("send now");
+      const response: InvokeModelCommandOutput = await client.send(request);
+
+      const decodedResponse = new TextDecoder().decode(response.body);
+
+      const reply = JSON.parse(decodedResponse || "{}");
+      const claudesReply = reply.content[0].text;
+      setSummary(claudesReply);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      // TODO:
+    }
+  };
 
   useEffect(() => {
     if (loading) {
@@ -57,10 +117,10 @@ export default function HomeScreen() {
         <Form
           gap="$2"
           onSubmit={() => {
-            setSummary("Test summary");
             setQuestion("test question");
             setPages([1, 2]);
             setLoading(true);
+            sendToBedrock();
           }}
         >
           <XStack alignItems="center" gap="$2">
