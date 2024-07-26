37f65a7a9d6811e82c5f4d0964d4945f84c9e8a8
Henry Fraser <henrymfraser@gmail.com>
2024-07-26 22:10:29 +1000

    Add react eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,38 @@
 import prettier from "eslint-plugin-prettier";
 import expo from "eslint-plugin-expo";
+import react from "eslint-plugin-react";
 import js from "@eslint/js";
 import tseslint from 'typescript-eslint';
+import globals from 'globals'
 
 
+// TODO: add react recommended
 export default [
     js.configs.recommended,
     ...tseslint.configs.recommended,
+    react.configs.flat.recommended,
     {
         ignores: [".expo", "**/node_modules"],
-    }, {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+
+            }
+        }
+    },
+    {
         plugins: {
             prettier,
             expo,
         },
-
         rules: {
             "prettier/prettier": "error",
 
             // expo only has these two rules
             "expo/no-env-var-destructuring": "error",
-            "expo/no-dynamic-env-var": "error"
+            "expo/no-dynamic-env-var": "error",
+            "react/jsx-uses-react": "off",
+            "react/react-in-jsx-scope": "off"
         },
     }];
